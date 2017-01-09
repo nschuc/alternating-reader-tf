443db04ad746023894121e5f7b0d039b4f097f9c
Nathan Schucher <nathan.schucher@gmail.com>
2017-01-09 01:38:10 -0500

    Add soft op placement and default gpu:0 for model

--- main.py
+++ main.py
@@ -46,8 +46,9 @@ print('Fixed Document length:', doc_len)
 print('Fixed Query length:', query_len)
 
 # Train Model
-with tf.Session() as sess:
-    model = AlternatingAttention(FLAGS.batch_size, vocab_size, doc_len, query_len, FLAGS.encoding_dim, FLAGS.embedding_dim, FLAGS.num_glimpses, session=sess)
+with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+    with tf.device('/gpu:0'):
+        model = AlternatingAttention(FLAGS.batch_size, vocab_size, doc_len, query_len, FLAGS.encoding_dim, FLAGS.embedding_dim, FLAGS.num_glimpses, session=sess)
     saver = tf.train.Saver(tf.global_variables())
 
     timestamp = str(datetime.now())
