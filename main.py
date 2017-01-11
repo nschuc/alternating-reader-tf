9a731ba670742fed5b1510ba58eb87efb40e946c
Nathan Schucher <nathan.schucher@gmail.com>
2017-01-11 00:40:10 -0500

    Fix checkpointing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pprint
 import tensorflow as tf
 import os
 from datetime import datetime
-from train import run
+import train
 from data_helper import load_data
 from model import AlternatingAttention
 
@@ -21,12 +21,13 @@ flags.DEFINE_float("learning_rate_decay", 0.8, "How much learning rate will deca
 flags.DEFINE_integer("batch_size", 32, "Batch Size (default: 32)")
 flags.DEFINE_integer("num_epochs", 12, "Number of training epochs (default: 12)")
 flags.DEFINE_integer("evaluate_every", 300, "Evaluate model on validation set after this many steps (default: 300)")
-flags.DEFINE_integer("checkpoint_every", 1000, "Save model after this many steps (default: 10000)")
 
 flags.DEFINE_boolean("trace", False, "Trace (load smaller dataset)")
 flags.DEFINE_string("log_dir", "logs", "Directory for summary logs to be written to default (./logs/)")
-flags.DEFINE_string("ckpt_dir", "ckpts", "Directory for checkpoints default (./ckpts/)")
 
+flags.DEFINE_integer("checkpoint_every", 1000, "Save model after this many steps (default: 1000)")
+flags.DEFINE_string("ckpt_dir", "ckpts", "Directory for checkpoints default (./ckpts/)")
+flags.DEFINE_string("restore_file", None, "Checkpoint to load")
 
 def main(_):
     FLAGS = tf.app.flags.FLAGS
@@ -39,31 +40,31 @@ def main(_):
     X_test, Q_test, Y_test = load_data('valid')
 
     vocab_size = np.max(X_train) + 1
-    doc_len = len(X_train[0])
-    query_len = len(Q_train[0])
+    print('[?] Vocabulary Size:', vocab_size)
 
-    print('Vocabulary Size:', vocab_size)
-    print('Fixed Document length:', doc_len)
-    print('Fixed Query length:', query_len)
-
-    timestamp = datetime.now().strftime('%c')
     # Create directories
-    FLAGS.ckpt_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
     if not os.path.exists(FLAGS.ckpt_dir):
         os.makedirs(FLAGS.ckpt_dir)
 
+    timestamp = datetime.now().strftime('%c')
     FLAGS.log_dir = os.path.join(FLAGS.log_dir, timestamp)
     if not os.path.exists(FLAGS.log_dir):
         os.makedirs(FLAGS.log_dir)
-
 
     # Train Model
     with tf.Session(config=tf.ConfigProto(log_device_placement=False, allow_soft_placement=True)) as sess, tf.device('/gpu:0'):
         model = AlternatingAttention(FLAGS.batch_size, vocab_size, FLAGS.encoding_dim, FLAGS.embedding_dim, FLAGS.num_glimpses, session=sess)
 
-        run(FLAGS, sess, model,
+        if FLAGS.trace: # Trace model for debugging
+            train.trace(FLAGS< sess, model, (X_train, Q_train, Y_train))
+            return
+
+        saver = tf.train.Saver()
+
+        train.run(FLAGS, sess, model,
                 (X_train, Q_train, Y_train),
-                (X_test, Q_test, Y_test))
+                (X_test, Q_test, Y_test),
+                saver)
 
 if __name__ == '__main__':
     tf.app.run()
--- train.py
+++ train.py
@@ -38,52 +38,60 @@ def run_epoch(model, X, Q, Y):
     total_loss /= batch_num
     return total_loss, total_accuracy
 
-def run(config, sess, model, train_data, test_data):
+def trace(config, sess, model, train_data):
+    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    run_metadata = tf.RunMetadata()
+
+    X, Q, Y = random_batch(*train_data, config.batch_size)
+    model.batch_fit(X, Q, Y, learning_rate, run_options, run_metadata)
+    train_writer.add_run_metadata(run_metadata, 'step%d' % step)
+
+    from tensorflow.python.client import timeline
+    tl = timeline.Timeline(run_metadata.step_stats)
+    ctf = tl.generate_chrome_trace_format()
+    with open('timeline.json', 'w') as f:
+        f.write(ctf)
+    return
+
+def run(config, sess, model, train_data, test_data, saver=None):
     X_train, Q_train, Y_train = train_data
     X_test, Q_test, Y_test = test_data
 
     train_writer = tf.summary.FileWriter(os.path.join(config.log_dir, 'train'), sess.graph)
     test_writer = tf.summary.FileWriter(os.path.join(config.log_dir, 'test'))
 
-
     learning_rate = config.learning_rate
     last_accuracy = 0
     valid_acc = 0
     half_epoch = 500 * (len(X_train) / (2 * config.batch_size) // 500)
+
+    if config.restore_file is not None:
+        print('[!] Loading variables from checkpoint %s' % config.restore_file)
+        saver.restore(sess, config.restore_file)
 
     num_batches = len(X_train) - len(X_train) % config.batch_size
     for epoch in range(config.num_epochs):
         for start in tqdm(range(0, num_batches, config.batch_size)):
             end = start + config.batch_size
             X, Q, Y = (X_train[start:end], Q_train[start:end], Y_train[start:end])
-
-            run_options = run_metadata = None
-            if config.trace:
-                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-                run_metadata = tf.RunMetadata()
-
             batch_loss, summary, step, attentions = model.batch_fit(
-                    X, Q, Y, learning_rate, run_options, run_metadata)
-
+                    X, Q, Y, learning_rate)
             train_writer.add_summary(summary, step)
-            if config.trace:
-                train_writer.add_run_metadata(run_metadata, 'step%d' % step)
-                from tensorflow.python.client import timeline
-                tl = timeline.Timeline(run_metadata.step_stats)
-                ctf = tl.generate_chrome_trace_format()
-                with open('timeline.json', 'w') as f:
-                    f.write(ctf)
-                return
             if step % config.evaluate_every == 0:
-                batch = random_batch(X_test, Q_test, Y_test, config.batch_size)
-                test_loss, summary, attentions = model.batch_predict(*batch)
+                X, Q, Y = random_batch(X_test, Q_test, Y_test, config.batch_size)
+                test_loss, summary, attentions = model.batch_predict(X, Q, Y)
                 accuracy = compute_accuracy(batch[0], attentions, batch[2])
                 last_accuracy = accuracy
                 test_writer.add_summary(summary, step)
+            if step % config.checkpoint_every == 0:
+                print('[!] Running epoch on validation set for checkpoint')
+                #loss, acc = run_epoch(model, X_test, Q_test, Y_test)
+                loss = acc = 0
+                ckpt_file = 'model-l{:.3f}_a{:.3f}.ckpt'.format(loss, acc)
+                path = saver.save(sess, os.path.join(config.ckpt_dir, ckpt_file), global_step=step)
+                print('[!] Saved checkpoint to %s' % path)
             if step % half_epoch == 0: # Compute loss over validation set
                 valid_loss, new_valid_acc = run_epoch(model, X_test, Q_test, Y_test)
                 if new_valid_acc >= valid_acc:
                     learning_rate = learning_rate * config.learning_rate_decay
                 valid_acc = new_valid_acc
-                path = saver.save(sess, checkpoint_prefix + '_{:.3f}_{:.3f}'.format(valid_loss, valid_acc), global_step=step)
-
